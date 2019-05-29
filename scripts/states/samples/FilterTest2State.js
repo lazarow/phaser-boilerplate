a5ce228ffeabc5ef6625f50bc11a8e9376c8c6f8
Arkadiusz Nowakowski <arkadiusz.nowakowski@us.edu.pl>
2019-05-29 19:30:55 +0200

    feat: samples

--- scripts/states/samples/FilterTest2State.js
+++ scripts/states/samples/FilterTest2State.js
@@ -4,23 +4,28 @@ class FilterTest2State extends Phaser.State
 {
     preload() {
         this.game.stage.backgroundColor = '#F0C873';
+        this.game.load.image('empty', '{ASSETS_PATH}/images/samples/filter-test2/empty.png');
         this.game.load.image('rock', '{ASSETS_PATH}/images/samples/filter-test2/rock.png');
+        this.game.load.image('white-bar', '{ASSETS_PATH}/images/samples/filter-test2/white-bar.png');
     }
     create() {
-        const rock = this.game.add.sprite(0, 0, 'rock');
+        const sprite1 = this.game.add.sprite(0, 0, 'white-bar');
+        const sprite2 = this.game.add.sprite(0, 32, 'empty');
+        const sprite3 = this.game.add.sprite(0, 64, 'rock');
         const filter = new Phaser.Filter(this.game, {
-            iChannel0: { type: 'sampler2D', value: rock.texture, textureData: { repeat: false } }
         }, `
 precision mediump float;
 varying vec2 vTextureCoord;
-uniform vec2 resolution;
+uniform vec2 resolution;        
 uniform sampler2D uSampler;
-uniform sampler2D iChannel0;
 void main( void ) {
-    gl_FragColor = texture2D(uSampler, vTextureCoord);
+    vec2 uv = gl_FragCoord.xy / resolution.xy;
+    gl_FragColor = vec4(1, 0, 0, uv.x);
 }
 `);
-        rock.filters = [filter];
+        sprite1.filters = [filter];
+        sprite2.filters = [filter];
+        sprite3.filters = [filter];
     }
 }
 
